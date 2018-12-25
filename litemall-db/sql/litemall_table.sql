9ca1e39ebb65c77b625a5bb22f30118be0d6c07a
Leon Ma <leonme@126.com>
2018-12-25 14:32:05 +0800

    Update for appid and SQL and server port to 8083

--- litemall-db/sql/litemall_table.sql
+++ litemall-db/sql/litemall_table.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: litemall
 -- ------------------------------------------------------
 -- Server version	5.7.21-log
-
+USE litemall;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
